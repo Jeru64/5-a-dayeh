464267a52ee2bd4125ffa90b23398a7ee20409f7
Jeru64 <cherish.jacob@gmail.com>
2026-07-31 19:44:21 +0400

    Add files via upload

---
+++ Digit-to-romen.py
@@ -0,0 +1,19 @@
+def int_to_roman(num):
+    values = [1000, 900, 500, 400,
+              100, 90, 50, 40,
+              10, 9, 5, 4, 1]
+    numerals = ["M", "CM", "D", "CD",
+                "C", "XC", "L", "XL",
+                "X", "IX", "V", "IV", "I"]
+    result = ""
+    i = 0
+    while num > 0:
+        if num >= values[i]:
+            result += numerals[i]
+            num -= values[i]
+        else:
+            i += 1
+    return result
+
+n = int(input("Enter a number: "))
+print(int_to_roman(n))
